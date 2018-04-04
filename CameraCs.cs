62a38e458305dea4528b6d9e8cba7f37fb728f47
XiAr322 <vlas19978@gmail.com>
2018-04-04 06:09:11 +0300

    camera 04.04.18 06:08
    simple camera

---
+++ CameraCs.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCs : MonoBehaviour {
+
+    public float panSpeed = 20f;
+    public float pBT = 10f;
+
+    void Update () {
+
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") )
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s") )
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d") )
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("a") )
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+	}
+}
